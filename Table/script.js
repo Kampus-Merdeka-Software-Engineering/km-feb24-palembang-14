2f9be60a5c3435d92206f52fa51f03f4f9e47ee4
SamuelHamdani <samuelhamdani04@gmail.com>
2024-06-11 10:49:27 +0700

    Added Filter for Datatable

--- Table/script.js
+++ Table/script.js
@@ -16,38 +16,70 @@ document.addEventListener("click", function (e) {
   }
 });
 
+// Table Function
 $(document).ready(function() {
-  // Mengambil data dari file JSON
+  // Fetch data from JSON
   $.getJSON('../data/data.json', function(data) {
-      // Menginisilaisasi data table
-      $('#table').DataTable({
-          data: data,
-          columns: [
-          { data: 'Row ID' },
-          { data: 'Order ID' },
-          { data: 'Order Date' },
-          { data: 'Ship Date' },
-          { data: 'Ship Mode' },
-          { data: 'Customer ID' },
-          { data: 'Customer Name' },
-          { data: 'Segment' },
-          { data: 'Country' },
-          { data: 'City' },
-          { data: 'State' },
-          { data: 'Postal Code' },
-          { data: 'Region' },
-          { data: 'Product ID' },
-          { data: 'Category' },
-          { data: 'Sub-Category' },
-          { data: 'Product Name' },
-          { data: 'Sales' },
-          { data: 'Quantity' },
-          { data: 'Discount' },
-          { data: 'Profit' }
-          ],
-          searching: true, //Mengaktifkan Fungsi Filtering / Pencarian
-          responsive: true, //Mengaktifkan Fungsi Responsive pada datatabless
-          scrollX: true, //Membuat tabel bisa digeser bukan dari layar tampilans
+    // Initialize DataTable
+    var table = $('#table').DataTable({
+      data: data,
+      columns: [
+        { data: 'Row ID' },
+        { data: 'Order ID' },
+        { data: 'Order Date' },
+        { data: 'Ship Date' },
+        { data: 'Ship Mode' },
+        { data: 'Customer ID' },
+        { data: 'Customer Name' },
+        { data: 'Segment' },
+        { data: 'Country' },
+        { data: 'City' },
+        { data: 'State' },
+        { data: 'Postal Code' },
+        { data: 'Region' },
+        { data: 'Product ID' },
+        { data: 'Category' },
+        { data: 'Sub-Category' },
+        { data: 'Product Name' },
+        { data: 'Sales' },
+        { data: 'Quantity' },
+        { data: 'Discount' },
+        { data: 'Profit' }
+      ],
+      searching: true,
+      responsive: true,
+      scrollX: true,
+    });
+
+    // Populate column filter
+    var columns = table.settings().init().columns; // retrieves the column settings from the DataTable.
+    columns.forEach((col, index) => {
+      $('#columnFilter').append(new Option(col.data, index));
+    });
+
+    // Get the value filter based on the selected column
+    $('#columnFilter').on('change', function() {
+      var columnIndex = $(this).val();
+      var columnData = table.column(columnIndex).data().unique().sort();
+      
+      // Disabling the value filter if the column is not being selected
+      $('#valueFilter').empty().append(new Option("Select Value", ""));
+      columnData.each(function(value) {
+        $('#valueFilter').append(new Option(value, value));
       });
+      $('#valueFilter').prop('disabled', columnIndex === "");
+    });
+
+    // Apply the second filter value based from the column name
+    $('#valueFilter').on('change', function() {
+      var columnIndex = $('#columnFilter').val();
+      var selectedValue = this.value;
+
+      if (selectedValue) {
+        table.column(columnIndex).search('^' + selectedValue + '$', true, false).draw();
+      } else {
+        table.column(columnIndex).search('').draw();
+      }
+    });
   });
 });
