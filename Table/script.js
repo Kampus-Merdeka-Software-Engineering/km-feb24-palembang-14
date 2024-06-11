295d3a03f38825b07b2886d53f040899ec8bb1ba
SamuelHamdani <samuelhamdani04@gmail.com>
2024-06-11 13:53:15 +0700

    Fixed navbar for datatable

--- Table/script.js
+++ Table/script.js
@@ -1,3 +1,19 @@
+// Toggle class active untuk hamburger menu
+const navbarNav = document.querySelector(".navbar-nav");
+// ketika hamburger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// Klik di luar elemen
+const hm = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
+
 $(document).ready(function() {
   // Fetch data from JSON
   $.getJSON('../data/data.json', function(data) {
